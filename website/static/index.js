f8258882f1f7df427299831a047140fc621473b2
valy3124 <valy3124@gmail.com>
2023-01-11 23:12:38 +0200

    Final version

--- website/static/index.js
+++ website/static/index.js
@@ -6,11 +6,3 @@ function deleteQuiz(quizId) {
       window.location.href = "/dashboard"; // redirect to the dashboard page
     });
 }
-
-function addQuestion() {
-    fetch("/add-question", {
-        method: "POST",
-    }).then((_res) => {
-        window.location.href = "/"; // redirect to the home page
-    })
-}
